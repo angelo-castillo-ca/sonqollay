90bc247de7101fe021d40252ce6e942b837c744b
angelo-castillo-ca <123595143+angelo-castillo-ca@users.noreply.github.com>
2024-05-26 18:23:08 -0500

    1

--- login.php
+++ login.php
@@ -6,12 +6,13 @@ ini_set('display_errors', 1);
 // Verificar si se ha enviado el formulario
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     include 'coneccion.php';
+
     // Recuperar los datos del formulario
     $email = $_POST["email"];
     $password = $_POST["password"];
 
     // Consulta para verificar las credenciales del usuario con consultas preparadas
-    $sql = "SELECT id, correo, passwd FROM usuario WHERE correo = ?";
+    $sql = "SELECT id, correo, passwd, rol FROM usuario WHERE correo = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("s", $email);
     $stmt->execute();
@@ -21,12 +22,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
         if (password_verify($password, $row['passwd'])) {
-            // Credenciales válidas, iniciar la sesión y guardar el ID
+            // Credenciales válidas, iniciar la sesión y guardar el ID y el rol del usuario
             session_start();
             $_SESSION['user_id'] = $row['id'];
+            $_SESSION['role'] = $row['rol'];
 
-            // Redirigir a la página del usuario
-            header("Location: user/confirmacion_ingreso.php");
+            // Redirigir según el rol del usuario
+            if ($row['rol'] == 'admin') {
+                header("Location: admin/confirmacion_ingreso.php");
+            } elseif ($row['rol'] == 'user') {
+                header("Location: user/index.php");
+            } else {
+                // En caso de que el rol no sea reconocido, redirigir a una página de error
+                header("Location: error.php");
+            }
             exit();
         } else {
             // Credenciales inválidas, mostrar mensaje de error
