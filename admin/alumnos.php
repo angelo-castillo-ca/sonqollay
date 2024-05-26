3a45fdc07bce1f4305e865b52da1c7c8204d54c1
Sauby21 <146679738+Sauby21@users.noreply.github.com>
2024-05-26 18:45:16 -0500

    1

--- admin/alumnos.php
+++ admin/alumnos.php
@@ -4,39 +4,27 @@ ini_set('display_errors', 1);
 
 // Inicia la sesión para acceder a la información de la sesión
 session_start();
-
-    // Ahora puedes utilizar $userId en tu consulta u otras operaciones
-    // Ejemplo de uso en una consulta
-    include '../coneccion.php';
-
-    $sql = "SELECT id, nombres,apellido_paterno,apellido_materno FROM usuario WHERE id = ?";
-    $stmt = $conn->prepare($sql);
-
-    if (!$stmt) {
-        die("Error en la preparación de la consulta: " . $conn->error);
-    }
-
-    $stmt->bind_param("i", $userId);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    // Procesar los resultados de la consulta
+    
+    // Consulta SQL para recuperar usuarios
+    $sql = "SELECT * FROM usuario";
+    
+    $result = $conn->query($sql);
+    
     if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-
-        // Almacena el nombre del usuario en una variable de sesión
-        $_SESSION['nombre_usuario'] = $row['nombres'];
-        $_SESSION['nombre_completo_usuario'] = $row['nombres'] . ' ' . $row['apellido_paterno'];
+        // Array para almacenar usuarios
+        $users = array();
+    
+        // Recorrer resultados y almacenar en el array
+        while($row = $result->fetch_assoc()) {
+            $users[] = $row;
+        }
+    
+        // Convertir array a formato JSON y devolverlo
+        echo json_encode($users);
     } else {
-        // Manejar el caso en que no se encuentra el usuario
-        echo "Error: No se encontró el usuario.";
+        echo "0 resultados";
     }
-
-    $stmt->close();
+    
     $conn->close();
-} else {
-    // El usuario no está autenticado, redirige a la página de inicio de sesión
-    header("Location: login.php");
-    exit();
-}
-?>
+    ?>
+    
