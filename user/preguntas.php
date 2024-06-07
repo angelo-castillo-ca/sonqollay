f7bcc320b92da5d32ebfc62da7b13a26e4dfe550
angelo-castillo-ca <123595143+angelo-castillo-ca@users.noreply.github.com>
2024-06-06 21:53:12 -0500

    1

--- user/preguntas.php
+++ user/preguntas.php
@@ -21,19 +21,77 @@ include 'back/session.php';
     <link rel="stylesheet" href="../assets/css/Simple-Slider-swiper-bundle.min.css">
     <link rel="stylesheet" href="../assets/css/Simple-Slider.css">
     <link rel="stylesheet" href="../assets/css/Steps-Progressbar.css">
-    <
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.user.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Droid+Serif">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    <link rel="stylesheet" href="../assets/css/swiper-icons.css">
+    <link rel="stylesheet" href="../assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="../assets/css/Data-Table-styles.css">
+    <link rel="stylesheet" href="../assets/css/Data-Table.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.2/css/theme.bootstrap_4.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css">
+    <link rel="stylesheet" href="../assets/css/Lista-Productos-Canito.css">
+    <link rel="stylesheet" href="../assets/css/Ludens---1-Index-Table-with-Search--Sort-Filters-v20.css">
+    <link rel="stylesheet" href="../assets/css/Navbar-Right-Links-icons.css">
+    <link rel="stylesheet" href="../assets/css/Navbar.css">
+    <link rel="stylesheet" href="../assets/css/Pricing-Table-with-Icon-Buy-Button-styles.css">
+    <link rel="stylesheet" href="../assets/css/Pricing-Table-with-Icon-Buy-Button.css">
+    <link rel="stylesheet" href="../assets/css/Simple-Slider-swiper-bundle.min.css">
+    <link rel="stylesheet" href="../assets/css/Simple-Slider.css">
+    <link rel="stylesheet" href="../assets/css/Steps-Progressbar.css">
 </head>
 <body>
-    <nav class="navbar navbar-expand-md bg-body py-3">
-        <div class="container"><a href="index.html"><img class="img-fluid" src="../assets/img/logos/LOGO SONQOLLAY.png" width="219" height="35"></a><a class="navbar-brand d-flex align-items-center" href="#"></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navcol-1">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link active" href="../registro.html"><button class="btn btn-primary" data-bss-hover-animate="rubberBand" type="button" style="background: rgb(255,180,29);text-align: center;margin: 0px;font-weight: bold;">EMPIEZA AHORA</button></a></li>
-                    <li class="nav-item"><a class="nav-link" href="../ingreso.html"><button class="btn btn-primary" data-bss-hover-animate="rubberBand" type="button" style="text-align: center;margin: 0px;font-weight: bold;">YA TENGO UNA CUENTA</button></a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+<nav class="navbar navbar-expand bg-white shadow mb-4 topbar static-top navbar-light" style="background: rgb(52,131,225);">
+    <div class="container-fluid">
+        <a href="index.php">
+            <img class="img-fluid" src="../assets/img/logos/LOGO SONQOLLAY.png" width="171" height="29">
+        </a>
+        <ul class="navbar-nav flex-nowrap ms-auto">
+            <li class="nav-item dropdown no-arrow">
+                <div class="nav-item dropdown no-arrow d-flex align-items-center">
+                    <a href="monedas.php" class="d-flex align-items-center">
+                        <span class="d-none d-lg-inline me-2 text-gray-600 small">
+                            <?php
+                                $creditos = $_SESSION['creditos'] ?? 0;
+                                echo "$" . htmlspecialchars($creditos);
+                            ?>
+                        </span>
+                        <img class="border rounded-circle img-profile img-fluid" src="../assets/img/monedas/pila-de-monedas.png" style="max-width: 30px; height: auto;">
+                    </a>
+                    <div class="d-none d-sm-block topbar-divider"></div>
+                    <a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#">
+                        <span class="d-none d-lg-inline me-2 text-gray-600 small">
+                            <?php
+                                $nombre_usuario = $_SESSION['nombre_usuario'] ?? '';
+                                echo htmlspecialchars($nombre_usuario);
+                            ?>
+                        </span>
+                        <img class="border rounded-circle img-profile" src="<?php echo htmlspecialchars($_SESSION['avatar_usuario']); ?>">
+                    </a>
+                    <div class="dropdown-menu shadow dropdown-menu-end animated--grow-in">
+                        <a class="dropdown-item" href="perfil.html">
+                            <i class="fas fa-user fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Perfil
+                        </a>
+                        <a class="dropdown-item" href="change-password.html">
+                            <i class="fas fa-user-lock fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Cambiar contraseña
+                        </a>
+                        <a class="dropdown-item" href="modulosA.html">
+                            <i class="fas fa-check-double fa-sm fa-fw me-2 text-gray-400"></i>Mis módulos&nbsp; aprobados
+                        </a>
+                        <a class="dropdown-item" href="modulos.html">
+                            <i class="fas fa-pencil-alt fa-sm fa-fw me-2 text-gray-400"></i>Mis módulos&nbsp; por llevar
+                        </a>
+                        <a class="dropdown-item" href="close.php">
+                            <i class="fas fa-sign-out-alt fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Salir
+                        </a>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
     <main>
     <section>
             <div class="steps-progressbar text-center container">
